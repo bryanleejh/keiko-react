00ce24fa652c7d8b304c64cd152a4e5238cbdbc1
bryan <bryan.lee.jh@gmail.com>
2020-03-15 15:54:16 +0800

    cardlist

--- src/index.js
+++ src/index.js
@@ -2,15 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Card from "./Card";
+import CardList from "./CardList";
 import * as serviceWorker from "./serviceWorker";
 import 'tachyons';
+import { robots } from './robots';
 
 ReactDOM.render(
-  <div>
-    <Card />
-    <Card />
-    <Card />
-  </div>,
+  <CardList robots={robots}/>,
   document.getElementById("root")
 );
 
