b2cf14d165f228de53828f6eeaf912354d17d3e8
bryan <bryan.lee.jh@gmail.com>
2020-03-21 10:47:47 +0800

    add fetch in app.js component did mount

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React, { Component } from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
-import { robots } from "./robots";
-import './App.css';
+// import { robots } from "./robots";
+import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ""
     };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response =>
+        response.json())
+      .then(users => {
+        this.setState({ robots: users });
+      });
   }
 
   onSearchChange = event => {
@@ -28,7 +37,7 @@ class App extends Component {
 
     return (
       <div className="tc">
-        <h1 className='f2'>RoboFriends</h1>
+        <h1 className="f2">RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <CardList robots={filteredRobots} />
       </div>
