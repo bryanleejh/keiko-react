0d1c4afbf529ea30a88a8e9bbf5312e74bfa71b2
bryan <bryan.lee.jh@gmail.com>
2020-03-22 10:42:46 +0800

    remove error

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,9 +2,10 @@ import React from "react";
 import Card from "./Card";
 
 const CardList = ({ robots }) => {
-  if(true) {
-    throw new Error ('Nooo');
-  }
+  // if(true) {
+  //   throw new Error ('Nooo');
+  // }
+  
   return (
     <div>
       {robots.map((user, i) => {
