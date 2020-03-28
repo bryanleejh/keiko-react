063a1f59dd9f958a145b7e7f405476420a32961a
bryan <bryan.lee.jh@gmail.com>
2020-03-28 16:40:10 +0800

    Updates

---
+++ precache-manifest.fd2a30be500bae047ea0278747135ce5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52275b61e4c99863e40f5f6868492cde",
+    "url": "/keiko-react/index.html"
+  },
+  {
+    "revision": "eb155a4f5db03ae9ac84",
+    "url": "/keiko-react/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "783f4aefe88b2d79904f",
+    "url": "/keiko-react/static/css/main.6f23527f.chunk.css"
+  },
+  {
+    "revision": "eb155a4f5db03ae9ac84",
+    "url": "/keiko-react/static/js/2.8af7d2d8.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/keiko-react/static/js/2.8af7d2d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "783f4aefe88b2d79904f",
+    "url": "/keiko-react/static/js/main.dafa3af2.chunk.js"
+  },
+  {
+    "revision": "ec51fc619dd04e51caaa",
+    "url": "/keiko-react/static/js/runtime-main.bf97482b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/keiko-react/static/media/SEGA.12a3db8b.woff"
+  }
+]);
